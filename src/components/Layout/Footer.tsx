5926fb64397a17aef4885907d0201e9be9ab9c8a
=Natty-7r <nati7fekadu@gmail.com>
2023-10-23 21:38:11 +0300

    footer text fixed

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-[black] sm:p-4 sm:py-8 text-white ">
       <div className="text-center my-4">
-        @2023 MogaCharity. All right reserved{" "}
+        @2023 Moga Charity. All right reserved{" "}
       </div>
       <div className="border-t text-xs w-[10%] mx-auto  border-white_dimmerflex my-4 "></div>
     </footer>
